b767239d424bfe30b039efbf9dc065520af645ec
bbritts <brunnobritts@hotmail.com>
2020-10-15 19:08:17 -0400

    Scanner

--- src/prova2/ProgramMain.java
+++ src/prova2/ProgramMain.java
@@ -1,11 +1,15 @@
 package prova2;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ProgramMain {
 
 	public static void main(String[] args) {
 		
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 
+		sc.close();
 	}
-
 }
